b5a4b5cd2f7641495ba441a8045028dc22797db1
dfg-98 <dariofg98@gmail.com>
2022-05-04 22:02:42 -0400

    refactor: prepare for conan packing

--- conanfile.py
+++ conanfile.py
@@ -1,42 +1,54 @@
-from conans import ConanFile, CMake
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 
 class HilbertplotCoreConan(ConanFile):
     name = "hilbertplot-core"
     version = "0.1"
+
+    # Optional metadata
     license = "MIT"
     author = "Dario Fragas Gonzalez dariofg98@gmail.com"
     url = "https://github.com/dfg-98/hilbertplot-core"
     description = "Generate Hilbert's curve and hilbert plots"
     topics = ("data-visualization", "data-minig")
+
+    requires = ["cmake/3.16.3", "fftw/3.3.9"]
+
+    # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = ["virtualenv", "cmake"]
-    requires = ["cmake/3.16.3", "fftw/3.3.9"]
-    exports_sources = "src/*"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+
+    scm = {
+        "type": "git",
+        "subfolder": "hilbertplot-core",
+        "url": "https://github.com/dfg-98/hilbertplot-core.git",
+        "revision": "master",
+    }
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure()
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="src")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["hilbertplot-core"]
---
+++ test_package/src/example.cpp
@@ -0,0 +1,5 @@
+#include "hilbertplot.h"
+
+int main() {
+    HilbertPlot h;
+}
